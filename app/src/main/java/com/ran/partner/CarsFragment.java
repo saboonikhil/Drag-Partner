43e4b10440e9b33761eb52ae3ec2c324c242e271
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2018-09-08 23:12:31 +0530

    Connect Add Car activity to Cars fragment
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

--- app/src/main/java/com/ran/partner/CarsFragment.java
+++ app/src/main/java/com/ran/partner/CarsFragment.java
@@ -1,9 +1,11 @@
 package com.ran.partner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
 public class CarsFragment extends Fragment {
 
     private Activity parentActivity;
+    private FloatingActionButton addCarButton;
 
     @Nullable
     @Override
@@ -24,5 +27,18 @@ public class CarsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         parentActivity.setTitle("My Cars");
+        initVariables();
+
+        addCarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(parentActivity, AddCarActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initVariables() {
+        addCarButton = parentActivity.findViewById(R.id.add_car_button);
     }
 }
