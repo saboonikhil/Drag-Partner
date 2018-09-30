a251dce4f465b658545f72209bf78a513479f21e
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2018-09-30 21:44:03 +0530

    Sync time layout in Now fragment
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

--- app/src/main/java/com/ran/partner/NowFragment.java
+++ app/src/main/java/com/ran/partner/NowFragment.java
@@ -9,10 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class NowFragment extends Fragment {
 
     private Activity parentActivity;
+    private Calendar now;
+    private LinearLayout timeLayout;
+    private TextView startTimeView, startDateView, endTimeView, endDateView;
     private Button actionNowButton;
 
     @Nullable
@@ -28,19 +38,38 @@ public class NowFragment extends Fragment {
         parentActivity.setTitle("RAN Partner");
         initVariables();
 
+        timeLayout.setVisibility(View.GONE);
         actionNowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (actionNowButton.getText().toString().equals("Ready To Pickup")) {
-                    actionNowButton.setText(R.string.start_trip);
-                } else if (actionNowButton.getText().toString().equals("Start Trip")) {
-                    actionNowButton.setText(R.string.end_trip);
+                switch (actionNowButton.getText().toString()) {
+                    case "Ready To Pickup":
+                        Toast.makeText(getContext(), "Message sent to the rider.", Toast.LENGTH_LONG).show();
+                        actionNowButton.setText(R.string.start_trip);
+                        break;
+                    case "Start Trip":
+                        timeLayout.setVisibility(View.VISIBLE);
+                        now = Calendar.getInstance();
+                        startTimeView.setText(new SimpleDateFormat("HH:mm", Locale.US).format(now.getTime()));
+                        startDateView.setText(new SimpleDateFormat("MMM d", Locale.US).format(now.getTime()));
+                        actionNowButton.setText(R.string.end_trip);
+                        break;
+                    case "End Trip":
+                        now = Calendar.getInstance();
+                        endTimeView.setText(new SimpleDateFormat("HH:mm", Locale.US).format(now.getTime()));
+                        endDateView.setText(new SimpleDateFormat("MMM d", Locale.US).format(now.getTime()));
+                        break;
                 }
             }
         });
     }
 
     private void initVariables() {
+        timeLayout = parentActivity.findViewById(R.id.now_time_layout);
+        startTimeView = parentActivity.findViewById(R.id.now_start_time);
+        startDateView = parentActivity.findViewById(R.id.now_start_date);
+        endTimeView = parentActivity.findViewById(R.id.now_end_time);
+        endDateView = parentActivity.findViewById(R.id.now_end_date);
         actionNowButton = parentActivity.findViewById(R.id.action_now_button);
     }
 }
