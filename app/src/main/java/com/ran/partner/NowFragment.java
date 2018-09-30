09492a5a76df74fa51fb260d84911b943af029c4
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2018-09-30 16:05:34 +0530

    Update action now button
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

--- app/src/main/java/com/ran/partner/NowFragment.java
+++ app/src/main/java/com/ran/partner/NowFragment.java
@@ -8,10 +8,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class NowFragment extends Fragment {
 
     private Activity parentActivity;
+    private Button actionNowButton;
 
     @Nullable
     @Override
@@ -24,5 +26,21 @@ public class NowFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         parentActivity.setTitle("RAN Partner");
+        initVariables();
+
+        actionNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (actionNowButton.getText().toString().equals("Ready To Pickup")) {
+                    actionNowButton.setText(R.string.start_trip);
+                } else if (actionNowButton.getText().toString().equals("Start Trip")) {
+                    actionNowButton.setText(R.string.end_trip);
+                }
+            }
+        });
+    }
+
+    private void initVariables() {
+        actionNowButton = parentActivity.findViewById(R.id.action_now_button);
     }
 }
