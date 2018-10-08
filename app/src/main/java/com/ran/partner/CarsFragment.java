df154301113bb230bd535d54b4daedba51abb4f5
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2018-10-09 00:21:21 +0530

    Scroll Date Picker connected to Cars fragment
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

--- app/src/main/java/com/ran/partner/CarsFragment.java
+++ app/src/main/java/com/ran/partner/CarsFragment.java
@@ -2,19 +2,27 @@ package com.ran.partner;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CarsFragment extends Fragment {
+import com.ran.partner.util.ScrollDatePicker.ScrollDatePicker;
+import com.ran.partner.util.ScrollDatePicker.listener.DateListener;
+
+import org.joda.time.DateTime;
+
+public class CarsFragment extends Fragment implements DateListener {
 
     private Activity parentActivity;
-    private FloatingActionButton addCarButton;
+    private ScrollDatePicker picker;
+    private FloatingActionButton addCarView;
 
     @Nullable
     @Override
@@ -29,7 +37,24 @@ public class CarsFragment extends Fragment {
         parentActivity.setTitle("My Cars");
         initVariables();
 
-        addCarButton.setOnClickListener(new View.OnClickListener() {
+        picker.setListener(this)
+                .setDays(30)
+                .setOffset(7)
+                .setDateSelectedColor(Color.DKGRAY)
+                .setDateSelectedTextColor(Color.WHITE)
+                .setMonthAndYearTextColor(Color.DKGRAY)
+                .setTodayButtonTextColor(getResources().getColor(R.color.colorPrimary))
+                .setTodayDateTextColor(getResources().getColor(R.color.colorPrimary))
+                .setTodayDateBackgroundColor(Color.GRAY)
+                .setUnselectedDayTextColor(Color.DKGRAY)
+                .setDayOfWeekTextColor(Color.DKGRAY)
+                .setUnselectedDayTextColor(getResources().getColor(R.color.primaryTextColor))
+                .showTodayButton(false)
+                .init();
+        picker.setBackgroundColor(Color.LTGRAY);
+        picker.setDate(new DateTime());
+
+        addCarView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(parentActivity, AddCarActivity.class);
@@ -38,7 +63,13 @@ public class CarsFragment extends Fragment {
         });
     }
 
+    @Override
+    public void onDateSelected(DateTime dateSelected) {
+        Log.i("ScrollDatePicker", "Show cars for " + dateSelected.toString());
+    }
+
     private void initVariables() {
-        addCarButton = parentActivity.findViewById(R.id.add_car_button);
+        addCarView = parentActivity.findViewById(R.id.add_car_button);
+        picker = parentActivity.findViewById(R.id.cars_scroll_date_picker);
     }
 }
