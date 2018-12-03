eea72e63071f2ffb9d1425bbbd7b4420d829d908
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2018-12-03 21:36:19 +0530

    Network updated
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

--- app/src/main/java/com/ran/partner/network/EndPointInterface.java
+++ app/src/main/java/com/ran/partner/network/EndPointInterface.java
@@ -1,5 +1,25 @@
 package com.ran.partner.network;
 
+import com.ran.partner.model.Cab;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface EndPointInterface {
 
+    @POST("/api/admin/cabs")
+    @FormUrlEncoded
+    Call<Cab> addCab(@Query("x_key") String key,
+                     @Query("token") String token,
+                     @Field("collegeName") String collegeName,
+                     @Field("pickup") String pickup,
+                     @Field("drop") String drop,
+                     @Field("startTime") String startTime,
+                     @Field("seats") String seats,
+                     @Field("fare") String fare,
+                     @Field("carName") String carName,
+                     @Field("startTime") String carNumber);
 }
