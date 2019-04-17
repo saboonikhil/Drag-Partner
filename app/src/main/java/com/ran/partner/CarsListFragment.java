3e2ad81fba7fd63ef3a1bca8ab42e17dc326299f
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2019-04-17 14:00:08 +0530

    Cars list adapter attached
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

--- app/src/main/java/com/ran/partner/CarsListFragment.java
+++ app/src/main/java/com/ran/partner/CarsListFragment.java
@@ -1,5 +1,7 @@
 package com.ran.partner;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,11 +13,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.ran.partner.adapter.CarsListAdapter;
+import com.ran.partner.model.Cab;
+import com.ran.partner.model.Partner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class CarsListFragment extends Fragment {
 
     private String date;
+    private Activity parentActivity;
     private View rootView;
     private RecyclerView recyclerView;
 
@@ -36,6 +47,7 @@ public class CarsListFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        parentActivity = getActivity();
         rootView = inflater.inflate(R.layout.fragment_cars_list, container, false);
         return rootView;
     }
@@ -45,10 +57,31 @@ public class CarsListFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initViews();
 
-        recyclerView.setAdapter(new CarsListAdapter(getContext()));
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        SharedPreferences pref = parentActivity.getSharedPreferences("AppPref", MODE_PRIVATE);
+        String json = pref.getString("dbObj", "");
+        Partner partner = new Gson().fromJson(json, Partner.class);
+
+        Cab[] trips = generateTripsData(partner.getCabs());
+        generateArrayData(trips);
 
         Toast.makeText(getContext(), date + " is selected!", Toast.LENGTH_SHORT).show();
+    }
+
+    private Cab[] generateTripsData(Cab[] cabs) {
+        ArrayList<Cab> myList = new ArrayList<>(Arrays.asList(cabs));
+        for (Cab cab : cabs) {
+            if (cab.isBooked()) {
+                myList.remove(cab);
+            }
+        }
+        Cab[] trips = new Cab[myList.size()];
+        return myList.toArray(trips);
+    }
+
+    private void generateArrayData(Cab[] cabs) {
+        CarsListAdapter carsListAdapter = new CarsListAdapter(cabs);
+        recyclerView.setAdapter(carsListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     private void initViews() {
