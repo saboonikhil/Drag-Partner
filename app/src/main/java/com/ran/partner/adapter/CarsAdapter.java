d6c1680ff3385abbd4aa32c4c6ed8595ee0c31d8
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2019-04-19 16:34:41 +0530

    Cars adapter updated
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

--- app/src/main/java/com/ran/partner/adapter/CarsAdapter.java
+++ app/src/main/java/com/ran/partner/adapter/CarsAdapter.java
@@ -1,32 +1,96 @@
 package com.ran.partner.adapter;
 
 import android.annotation.SuppressLint;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.ran.partner.CarsListFragment;
+import com.ran.partner.R;
+import com.ran.partner.model.Cab;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
-public class CarsAdapter extends FragmentPagerAdapter {
+public class CarsAdapter extends RecyclerView.Adapter<CarsAdapter.CarsViewHolder> {
 
-    private Calendar date;
+    private Cab[] cabs;
 
-    public CarsAdapter(FragmentManager fm, Calendar date) {
-        super(fm);
-        this.date = date;
+    public CarsAdapter(Cab[] cabs) {
+        this.cabs = cabs;
+    }
+
+    public void refreshData(Cab[] dataSet) {
+        cabs = dataSet;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public CarsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.layout_cars, parent, false);
+        return new CarsViewHolder(view);
     }
 
     @SuppressLint("SimpleDateFormat")
     @Override
-    public Fragment getItem(int position) {
-        return CarsListFragment.newInstance(new SimpleDateFormat("EEE, MMM d, yyyy").format(date.getTime()));
+    public void onBindViewHolder(@NonNull CarsViewHolder holder, int position) {
+        holder.carNameView.setText(cabs[position].getCarName());
+        holder.carNumberView.setText(cabs[position].getCarNumber());
+
+        String pickup = cabs[position].getPickup();
+        if (TextUtils.isEmpty(pickup))
+            pickup = "All Pickup Locations";
+        holder.pickupView.setText(pickup);
+
+        String drop = cabs[position].getDrop();
+        if (TextUtils.isEmpty(drop))
+            drop = "All Drop Locations";
+        holder.dropView.setText(drop);
+
+        holder.seatsView.setText(cabs[position].getSeats());
+        holder.fareView.setText(cabs[position].getFare());
+
+        if (cabs[position].getStartTime() != null) {
+            try {
+                Calendar calendar = Calendar.getInstance();
+                Date displayTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(cabs[position].getStartTime());
+                calendar.setTime(displayTime);
+                calendar.add(Calendar.HOUR, 5);
+                calendar.add(Calendar.MINUTE, 30);
+                String startTime = new SimpleDateFormat("hh:mm a").format(calendar.getTime());
+                if (startTime.equals("12:00 PM"))
+                    startTime = "Any";
+                holder.startTimeView.setText(startTime);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
-    public int getCount() {
-        return 31;
+    public int getItemCount() {
+        return cabs.length;
+    }
+
+    class CarsViewHolder extends RecyclerView.ViewHolder {
+        private TextView carNameView, carNumberView, pickupView, dropView, seatsView, fareView, startTimeView;
+
+        CarsViewHolder(View itemView) {
+            super(itemView);
+            carNameView = itemView.findViewById(R.id.cars_car_name);
+            carNumberView = itemView.findViewById(R.id.cars_car_number);
+            pickupView = itemView.findViewById(R.id.cars_pickup);
+            dropView = itemView.findViewById(R.id.cars_drop);
+            startTimeView = itemView.findViewById(R.id.cars_time);
+            fareView = itemView.findViewById(R.id.cars_fare);
+            seatsView = itemView.findViewById(R.id.cars_seats);
+        }
     }
 }
