3f4cacb6e2388fb98ebb8dafc2f602efe3456258
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2019-05-24 11:54:52 +0530

    User added
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

---
+++ app/src/main/java/com/ran/partner/model/User.java
@@ -0,0 +1,28 @@
+package com.ran.partner.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String name;
+    private String contact;
+    private String alternateContact;
+
+    public User(String name, String contact, String alternateContact) {
+        this.name = name;
+        this.contact = contact;
+        this.alternateContact = alternateContact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getAlternateContact() {
+        return alternateContact;
+    }
+}
