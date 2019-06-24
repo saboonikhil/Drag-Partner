812e8c6a6f0c1dbf3148ff293af7a718db1f6a26
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2019-06-24 14:39:59 +0530

    Network security config added
    Signed-off-by: Nikhil Saboo <ns.nikhilsaboo@gmail.com>

--- app/src/main/java/com/drag/partner/network/APIUtils.java
+++ app/src/main/java/com/drag/partner/network/APIUtils.java
@@ -2,7 +2,7 @@ package com.drag.partner.network;
 
 public class APIUtils {
 
-    private static final String BASE_URL = "https://a3cc5de8.ngrok.io";
+    private static final String BASE_URL = "http://ec2-52-66-214-151.ap-south-1.compute.amazonaws.com:8080";
 
     public static EndPointInterface getAPIService() {
         return RetrofitClientInstance.getRetrofitInstance(BASE_URL).create(EndPointInterface.class);
